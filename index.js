4d284dad47beebb401f10a762cd96901eac0229f
il3ven <vaibhavchanana@yahoo.co.in>
2021-12-30 17:40:11 +0530

    add post now working

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const peer = "http://localhost:26657";
 
+const errorId = "error";
+const postId = "posts";
+const succcessId = "success";
+
 function hexToBytes(hex) {
   for (var bytes = [], c = 0; c < hex.length; c += 2)
     bytes.push(parseInt(hex.substr(c, 2), 16));
@@ -12,6 +16,11 @@ function ab2hex(buf) {
   return [...new Uint8Array(buf)]
     .map((v) => v.toString(16).padStart(2, "0"))
     .join("");
+}
+
+function uuid() {
+  const uint32 = crypto.getRandomValues(new Uint32Array(1))[0];
+  return uint32.toString(16);
 }
 
 async function fetchAllPosts() {
@@ -49,7 +58,7 @@ async function fetchAllPosts() {
         const tx = atob(tx64);
         const [id, jsonString] = tx.split("=");
         const msg = JSON.parse(jsonString);
-        return msg?.msg;
+        return msg;
       })
     );
   }
@@ -57,33 +66,79 @@ async function fetchAllPosts() {
   return posts;
 }
 
-async function addPost(post, privHexString) {
-  const enc = new TextEncoder();
-  const priv = await crypto.subtle.importKey(
-    "pkcs8",
-    hexToBytes(privHexString),
-    {
+async function addPost(formId) {
+  try {
+    const enc = new TextEncoder();
+    const form = new FormData(document.getElementById(formId));
+    const privHexString = form.get("private-key");
+    const post = form.get("add-post");
+
+    const algo = {
       name: "ECDSA",
       namedCurve: "P-384",
-    },
-    false,
-    ["sign"]
-  );
+    };
 
-  const _sign = await crypto.subtle.sign(
-    { name: "ECDSA", hash: "SHA-256" },
-    priv,
-    enc.encode(msg)
-  );
-  const sign = ab2hex(_sign);
+    const _priv = await crypto.subtle.importKey(
+      "pkcs8",
+      hexToBytes(privHexString),
+      algo,
+      true,
+      ["sign"]
+    );
+    const jwk = await crypto.subtle.exportKey("jwk", _priv);
+    delete jwk.d;
+    jwk.key_ops = ["verify"];
 
-  
+    const pub = await crypto.subtle.importKey("jwk", jwk, algo, true, [
+      "verify",
+    ]);
+    const _pub = await crypto.subtle.exportKey("spki", pub);
+
+    const _sign = await crypto.subtle.sign(
+      { name: "ECDSA", hash: "SHA-256" },
+      _priv,
+      enc.encode(post)
+    );
+
+    const url =
+      `${peer}/broadcast_tx_sync?tx=` +
+      `"${uuid()}=${JSON.stringify({
+        publicKey: ab2hex(_pub),
+        signature: ab2hex(_sign),
+        msg: post,
+      }).replace(/\"/g, '\\"')}"`;
+
+    const resp = await fetch(url);
+
+    if (!resp.ok) throw new Error("Couldn't broadcasting transaction");
+
+    const rpc = await resp.json();
+    if (!rpc || rpc.result?.code != 0) throw new Error("Transaction rejected");
+
+    document.getElementById(succcessId).innerText =
+      "📢 Transaction broadcasted successfully. Wait for the transaction to be confirmed, refresh the page after a few seconds.";
+  } catch (err) {
+    const errBox = document.getElementById(errorId);
+    errBox.innerText = err.stack;
+  }
 }
 
 function renderPosts(posts, id) {
   const ul = document.getElementById(id);
 
-  ul.innerHTML = posts.map((post) => `<li>${post}</li>`).join("");
+  ul.innerHTML = posts
+    .map(
+      ({ msg, publicKey }) => `
+      <li style="padding-bottom: 3rem; border-top: 1px dotted #80808094;">
+        <p>${msg}</p>
+        <div style="display: flex; align-items: center;">
+          <span style="white-space: nowrap;">Author's Public Key:&nbsp&nbsp</span>
+          <pre style="overflow: scroll; display:inline-block; margin: 0;">${publicKey}</pre>
+        </div>
+      </li>
+    `
+    )
+    .join("");
 }
 
 function generateKeyPair() {
@@ -123,47 +178,47 @@ const enc = new TextEncoder();
 const dec = new TextDecoder();
 
 (async () => {
-  // const posts = await fetchAllPosts()
-  console.log("Posts", posts);
+  const posts = await fetchAllPosts();
+  console.log("Fetched Posts", posts);
   renderPosts(posts, "posts");
 
-  const _priv = hexToBytes(
-    "3081b6020100301006072a8648ce3d020106052b8104002204819e30819b02010104302a0131d13b906af21afa4f220368276fa508802e413269e5ce53f08c8ec7e857456471624bbabf4ea85f7e8d47661556a164036200040b80a84bcf25ee5d415425160d4c5f3a3a056c1c95933fd5b277f8986f7660e1e5a25a53f0a402cb1890dfbe7182b5f16b2aa48e8581fdac6532cba3c35e795204aec9e0776d6042b4fdc7d7f7d44f6619612037acee731e21dd3df65130ea66"
-  );
-  const _pub = hexToBytes(
-    "3076301006072a8648ce3d020106052b8104002203620004629d52c2bedabbac9f2663a9e85fef37c29748645d622d5538cb6d9eb374db6836c2124b965e2cfb8707bc95f7803e83738afd090e61da79060760c0f7ed08b2d999dfc5db01599c7d4e888e01643354808e02996fc0a770215a15ca832f458d"
-  );
-  let sign = hexToBytes(
-    "3065023054aeb5083aba468d0287be67c09abc6bfcb3569ce12746520f1e9b2dc9cef69dae0f38896ed9f6b9a614d791cdebf49d0231009f0a448c323f60f6f601a0ee0b1ba7e6a349a321cd93ca585a923b5c1f3acf072a7f5be0c2f1f2e394d12e7eeb43f4d2"
-  );
-  const msg = enc.encode("some data to sign");
+  // const _priv = hexToBytes(
+  //   "3081b6020100301006072a8648ce3d020106052b8104002204819e30819b02010104302a0131d13b906af21afa4f220368276fa508802e413269e5ce53f08c8ec7e857456471624bbabf4ea85f7e8d47661556a164036200040b80a84bcf25ee5d415425160d4c5f3a3a056c1c95933fd5b277f8986f7660e1e5a25a53f0a402cb1890dfbe7182b5f16b2aa48e8581fdac6532cba3c35e795204aec9e0776d6042b4fdc7d7f7d44f6619612037acee731e21dd3df65130ea66"
+  // );
+  // const _pub = hexToBytes(
+  //   "3076301006072a8648ce3d020106052b8104002203620004629d52c2bedabbac9f2663a9e85fef37c29748645d622d5538cb6d9eb374db6836c2124b965e2cfb8707bc95f7803e83738afd090e61da79060760c0f7ed08b2d999dfc5db01599c7d4e888e01643354808e02996fc0a770215a15ca832f458d"
+  // );
+  // let sign = hexToBytes(
+  //   "3065023054aeb5083aba468d0287be67c09abc6bfcb3569ce12746520f1e9b2dc9cef69dae0f38896ed9f6b9a614d791cdebf49d0231009f0a448c323f60f6f601a0ee0b1ba7e6a349a321cd93ca585a923b5c1f3acf072a7f5be0c2f1f2e394d12e7eeb43f4d2"
+  // );
+  // const msg = enc.encode("some data to sign");
 
-  const algo = { name: "ECDSA", namedCurve: "P-384" };
+  // const algo = { name: "ECDSA", namedCurve: "P-384" };
   // let priv = await crypto.subtle.importKey("pkcs8", _priv, algo, true, [
   //   "sign",
   // ]);
   // let pub = await crypto.subtle.importKey("spki", _pub, algo, true, [
   //   "verify",
   // ]);
-  let { privateKey: priv, publicKey: pub } = await crypto.subtle.generateKey(
-    {
-      name: "ECDSA",
-      namedCurve: "P-384",
-    },
-    true,
-    ["sign", "verify"]
-  );
-  const exportedPub = await crypto.subtle.exportKey("spki", pub);
-  const exportedPriv = await crypto.subtle.exportKey("pkcs8", priv);
+  // let { privateKey: priv, publicKey: pub } = await crypto.subtle.generateKey(
+  //   {
+  //     name: "ECDSA",
+  //     namedCurve: "P-384",
+  //   },
+  //   true,
+  //   ["sign", "verify"]
+  // );
+  // const exportedPub = await crypto.subtle.exportKey("spki", pub);
+  // const exportedPriv = await crypto.subtle.exportKey("pkcs8", priv);
 
-  console.log("Private Key", ab2hex(exportedPriv));
-  console.log("Public Key", ab2hex(exportedPub));
+  // console.log("Private Key", ab2hex(exportedPriv));
+  // console.log("Public Key", ab2hex(exportedPub));
 
-  const signAlgo = { name: "ECDSA", hash: "SHA-256" };
-  sign = await crypto.subtle.sign(signAlgo, priv, msg);
-  console.log("Signature", ab2hex(sign));
+  // const signAlgo = { name: "ECDSA", hash: "SHA-256" };
+  // sign = await crypto.subtle.sign(signAlgo, priv, msg);
+  // console.log("Signature", ab2hex(sign));
 
-  let signValid = await window.crypto.subtle.verify(signAlgo, pub, sign, msg);
+  // let signValid = await window.crypto.subtle.verify(signAlgo, pub, sign, msg);
 
-  console.log(signValid);
+  // console.log(signValid);
 })();
